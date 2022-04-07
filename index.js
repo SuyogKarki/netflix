a18d4e888f3a5786d8303eac4597dd1c31a7f2c0
Suyog Karki <karkisuyog897@gmail.com>
2022-04-07 10:48:32 +0545

    v-1.02

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
     return res.status(200).json({});
   }
+  next();
 });
 app.use(express.json());
 app.use('/api/auth', authRoute);
